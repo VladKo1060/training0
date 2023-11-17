4dc4b67cbac320bc44f9996685f34e3cebb0a60e
VladKo1060 <134947972+VladKo1060@users.noreply.github.com>
2023-11-17 16:43:38 +0300

    Файл машины состояний
    Машина состояний готова к работе. Диалог регистрации. Спрашивает Имя, Фамилию, Дату рождения. Пока нет фильтрации входных данных. Можно запускать и как самостоятельного бота, и как импортируемый файл.

--- FSM.py
+++ FSM.py
@@ -5,6 +5,8 @@ from telegram.ext import Updater, MessageHandler, Dispatcher, CallbackContext, F
 import logging
 from key import TOKEN
 
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 WAIT_NAME, WAIT_SURNAME, WAIT_BIRTHDAY = range(3)
@@ -14,6 +16,7 @@ def ask_name(update: Update, context: CallbackContext):
     user_id = update.message.from_user.id
     user_name = update.message.from_user.name
     logging.info(f'{user_id=}, {user_name=},  вызвал функцию ask_name')
+    update.message.reply_text(f'Назови своё имя')
 
     return WAIT_NAME
 
@@ -22,6 +25,7 @@ def get_name(update: Update, context: CallbackContext):
     user_id = update.message.from_user.id
     user_name = update.message.from_user.name
     logging.info(f'{user_id=}, {user_name=},  вызвал функцию get_name')
+    update.message.reply_text(f'Получил твоё имя')
 
     return ask_surname(update, context)
 
@@ -30,6 +34,7 @@ def ask_surname(update: Update, context: CallbackContext):
     user_id = update.message.from_user.id
     user_name = update.message.from_user.name
     logging.info(f'{user_id=}, {user_name=},  вызвал функцию ask_surname')
+    update.message.reply_text(f'Назови свою фамилию')
 
     return WAIT_SURNAME
 
@@ -38,6 +43,7 @@ def get_surname(update: Update, context: CallbackContext):
     user_id = update.message.from_user.id
     user_name = update.message.from_user.name
     logging.info(f'{user_id=}, {user_name=},  вызвал функцию get_surname')
+    update.message.reply_text(f'Получил твою фамилию')
 
     return ask_birthday(update, context)
 
@@ -46,6 +52,7 @@ def ask_birthday(update: Update, context: CallbackContext):
     user_id = update.message.from_user.id
     user_name = update.message.from_user.name
     logging.info(f'{user_id=}, {user_name=},  вызвал функцию ask_birthday')
+    update.message.reply_text(f'Назови свой день рождения')
 
     return WAIT_BIRTHDAY
 
@@ -54,6 +61,7 @@ def get_birthday(update: Update, context: CallbackContext):
     user_id = update.message.from_user.id
     user_name = update.message.from_user.name
     logging.info(f'{user_id=}, {user_name=},  вызвал функцию get_birthday')
+    update.message.reply_text(f'Получил твой день рождения')
 
     return register(update, context)
 
@@ -62,5 +70,24 @@ def register(update: Update, context: CallbackContext):
     user_id = update.message.from_user.id
     user_name = update.message.from_user.name
     logging.info(f'{user_id=}, {user_name=},  вызвал функцию register')
+    update.message.reply_text(f'Зарегестрировал тебя')
 
     return ConversationHandler.END
+
+
+
+conversation_handler = ConversationHandler(
+    entry_points=[CommandHandler('register', ask_name)],
+    states={WAIT_NAME: [MessageHandler(Filters.text, get_name)],
+            WAIT_SURNAME: [MessageHandler(Filters.text, get_surname)],
+            WAIT_BIRTHDAY: [MessageHandler(Filters.text, get_birthday)]
+            },
+    fallbacks=[]
+)
+
+if __name__ == '__main__':
+    updater = Updater(token=TOKEN)
+    dispatcher: Dispatcher = updater.dispatcher
+    dispatcher.add_handler(conversation_handler)
+    updater.start_polling()
+    updater.idle()
